e6270dde105109b5274a17786fbd79a750721988
Jonathan Rubin <jonathan.a.rubin@gmail.com>
2018-04-17 18:57:15 -0400

    Guess function is now correctly changing DOM elements

--- script.js
+++ script.js
@@ -1,40 +1,75 @@
-const collectionOfWords = [{
-    word: "test1ruby",
-    hint: "hint1"
-},
-{
-    word: "test2python",
-    hint: "hint2"
-},
-{
-    word: "test3elixir",
-    hint: "hint3"
-},
-{
-    word: "test4fortran",
-    hint: "hint4"
-},
-{
-    word: "test5java",
-    hint: "hint5"
-},
-{
-    word: "test2csharp",
-    hint: "hint6"
-}]
+const word =
+    {
+        currentWord: [],
+        currentHint: "",
+
+        selectWordRandomly: function () {
+            const randomIndex = Math.floor(Math.random() * this.collectionOfWords.length)
+            this.collectionOfWords[randomIndex].word.split('').forEach((letter) => {
+                this.currentWord.push({ letter: letter, showing: false })
+            })
+            this.currentHint = this.collectionOfWords[randomIndex].hint
+        },
+
+        collectionOfWords: [{
+            word: "testruby",
+            hint: "hint1"
+        },
+        {
+            word: "testpython",
+            hint: "hint2"
+        },
+        {
+            word: "testelixir",
+            hint: "hint3"
+        },
+        {
+            word: "testfortran",
+            hint: "hint4"
+        },
+        {
+            word: "testjava",
+            hint: "hint5"
+        },
+        {
+            word: "testcsharp",
+            hint: "hint6"
+        }],
+
+
+    }
 
 const guesses = {
     remaining: 10,
-    incorrectLetters: []
-}
+    incorrectLetters: [],
+    makeGuess: function () {
+        $('.alphabet-letter').on('click', function () {
 
+            const guessedLetter = $(this).text()
+            
+            // loop thru array of letter objects
+            let letterFound = false
+            word.currentWord.forEach((letter, index) => {
 
-const selectWordRandomly = (words) => {
-    const randomIndex = Math.floor(Math.random() * words.length);
-    const selectedWord = words[randomIndex].word.split('').map((letter) => {
-        return { letter: letter, showing: false }
-    })
-    return selectedWord
+                // if the clicked on letter === current letter in loop
+                if(letter.letter === guessedLetter) {
+                    $(`#word-letter-${index}`).text(letter.letter)
+                    letterFound = true
+                    console.log("LETTER",letter.letter)
+                }               
+            })
+            if (!letterFound) {
+                addLetterToIncorrect(guessedLetter)
+                guesses.remaining -= 1
+                guesses.incorrectLetters.push(guessedLetter)
+            }
+            console.log(guesses.remaining);
+            
+            $(this).fadeOut(1000)
+
+            
+        })
+    }
 }
 
 const attemptToGuessLetter = (guessedLetter, word) => {
@@ -49,7 +84,7 @@ const attemptToGuessLetter = (guessedLetter, word) => {
 
 // DOM Manipulation
 
-const showAlphabetLetters = () => {
+const showAllAlphabetLetters = () => {
     const alphabetArray = 'abcdefghijklmnopqrstuvwxyz'.split('')
     alphabetArray.forEach((letter, index) => {
         $('#alphabet-id').append(`<div id="alpha-letter-${index}" class="alphabet-letter">${letter}<div>`)
@@ -63,42 +98,34 @@ const createLetterDivs = (word) => {
 }
 
 const addLetterToIncorrect = (letter) => {
-    $('.incorrect-guesses').append(`<div>${letter}</div>`)
+    $('.incorrect-guesses').append(`<div class="incorrect-letter">${letter}</div>`)
 }
 
-
-const makeGuess = () => {
-    $('.alphabet-letter').on('click', function () {
-        const guessedLetter = $(this).text()
-        guesses.remaining -= 1
-        console.log(guesses.remaining);
-        $(this).remove()
-        guesses.incorrectLetters.push(guessedLetter)        
-        addLetterToIncorrect(guessedLetter)
-    })
-}
-
-
-
-
-
-const randomlySelectedWord = selectWordRandomly(collectionOfWords)
-const currentWordAfterGuess = attemptToGuessLetter("t", randomlySelectedWord)
+word.selectWordRandomly()
 
 $(document).ready(function () {
-    showAlphabetLetters()
 
-    createLetterDivs(randomlySelectedWord)
-    
-    makeGuess()
+
+    showAllAlphabetLetters()
+
+    createLetterDivs(word.currentWord)
+
+    guesses.makeGuess()
 });
 
 
-// const showDivsForLetters = (word) => {
-//     if (letter.showing) {
-//         $('#current-word-id').append(`<div class="word-letter">${letter.letter}<div>`)
-//     }
-//     else {
-//         $('#current-word-id').append(`<div class="word-letter"><div>`)
-//     }
+// const showDivsForLetters = (hangmanWord) => {
+
+//     hangmanWord.forEach((letter) => {
+//         if (hangmanWord.showing) {
+//             $('#current-word-id').append(`<div class="word-letter">${letter.letter}<div>`)
+//         } else {
+//             $('#current-word-id').append(`<div class="word-letter"><div>`)            
+//         }
+//     })
+
+
+//     // else {
+//     //     
+//     // }
 // }
